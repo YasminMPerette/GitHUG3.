66b56c68b663baf2d1717f67d3f61e6c74ae831e
yasmin_perette <yasmin_perette@SN-360374.edu.fiesc.com.br>
2024-07-08 21:21:34 -0300

    hj

--- meu.projetoGitHUG.3/src/GitHUG/GitHUG3.java
+++ meu.projetoGitHUG.3/src/GitHUG/GitHUG3.java
@@ -3,7 +3,7 @@ package GitHUG;
 public class GitHUG3 {
 
 	public static void main(String[] args) {
-		  System.out.println ("Digite o tamanho de espaço para sua caixa: ");
+		  System.out.println ("Digite o tamanho de espaço para sa caixa: ");
 
 	}
 
